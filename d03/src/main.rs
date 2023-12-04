c626e32d3c37fce76d6e142640bcb9ed3673550f
fargito <fr.farge96@gmail.com>
2023-12-04 12:37:38 +0100

    feat: day3 part2

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -3,10 +3,12 @@ use helpers::{lazy_static, read_lines, Regex};
 lazy_static! {
     static ref NUMBER_REGEX: Regex = Regex::new("(?<number>\\d+)").unwrap();
     static ref SPECIAL_CHAR_REGEX: Regex = Regex::new("[^\\d\\.]").unwrap();
+    static ref GEAR_REGEX: Regex = Regex::new("\\*").unwrap();
 }
 
 fn main() {
     let mut result = 0;
+    let mut gear_result = 0;
     let mut lines: Vec<String> = vec![];
 
     if let Ok(lines_iter) = read_lines("./d03/inputs/input.txt") {
@@ -49,7 +51,28 @@ fn main() {
                 }
             }
         }
+
+        for gear_match in GEAR_REGEX.find_iter(&line) {
+            let gear_pos = gear_match.start();
+            let mut gear_num_count = 0;
+            let mut gear_count = 1;
+
+            // check if the gear is ok by looking all the possible lines
+            for line in lines_to_search.iter() {
+                for number_match in NUMBER_REGEX.find_iter(&line) {
+                    if number_match.start() <= gear_pos + 1 && number_match.end() >= gear_pos {
+                        gear_num_count += 1;
+                        gear_count *= number_match.as_str().parse::<u64>().unwrap();
+                    }
+                }
+            }
+
+            if gear_num_count == 2 {
+                gear_result += gear_count;
+            }
+        }
     }
 
-    println!("{result}");
+    println!("result {result}");
+    println!("gear {gear_result}");
 }
